3eaac1124cb3c762f2bd24743eeaeefda1928863
Nemanja Dicic <nemanja.dicic@gmail.com>
2020-01-21 15:45:32 +0100

    Wire up complete HomeView
    Connects controls on HomeView with their respective properties on HomeViewModel. Implements button methods and creates dummy TournamentModel data for testing purposes

--- CustomTournamentsUI/ViewModels/HomeViewModel.cs
+++ CustomTournamentsUI/ViewModels/HomeViewModel.cs
@@ -9,6 +9,101 @@ namespace CustomTournamentsUI.ViewModels
 {
     public class HomeViewModel : Screen
     {
+        //          Backing fields
+        private BindableCollection<TournamentModel> _existingTournaments;
+        private TournamentModel _selectedTournament;
+        private bool _canLoadTournament;
 
+
+
+
+
+        //          Properties
+        public BindableCollection<TournamentModel> ExistingTournaments
+        {
+            get { return _existingTournaments; }
+            set 
+            { 
+                _existingTournaments = value;
+                NotifyOfPropertyChange(() => ExistingTournaments);
+            }
+        }
+        public TournamentModel SelectedTournament
+        {
+            get { return _selectedTournament; }
+            set 
+            { 
+                _selectedTournament = value;
+                NotifyOfPropertyChange(() => SelectedTournament);
+
+                if (SelectedTournament != null)
+                {
+                    CanLoadTournament = true;
+                }
+                else
+                {
+                    CanLoadTournament = false;
+                }
+            }
+        }
+        public bool CanLoadTournament
+        {
+            get { return _canLoadTournament; }
+            set 
+            { 
+                _canLoadTournament = value;
+                NotifyOfPropertyChange(() => CanLoadTournament);
+            }
+        }
+
+
+
+
+
+
+        //          Methods and constructor
+        public void LoadTournament()
+        {
+            //TournamentModel tournament = SelectedTournament;
+
+            //var conductor = Parent as IConductor;
+            //if (tournament.IsLeague)
+            //{
+            //    conductor.ActivateItem(new LeagueViewModel(tournament));
+            //}
+            //else
+            //{
+            //    conductor.ActivateItem(new CupViewModel(tournament));
+            //}
+
+            // TODO - Uncomment when CupViewModel (and CupView) and LeagueViewModel (and LeagueView) are ready
+        }
+        
+        public void CreateNewTournament()
+        {
+            var conductor = Parent as IConductor;
+            conductor.ActivateItem(new CreateTournamentViewModel());
+        }
+
+        public HomeViewModel()
+        {
+            List<TournamentModel> tournaments = new List<TournamentModel>();
+            tournaments.Add(new TournamentModel { TournamentName = "Ping Pong SuperLiga Srbije", IsLeague = false });
+            tournaments.Add(new TournamentModel { TournamentName = "Besmisleni Kup", IsLeague = false });
+            tournaments.Add(new TournamentModel { TournamentName = "Topli Drugari", IsLeague = true });
+            tournaments.Add(new TournamentModel { TournamentName = "Izbori 2020.", IsLeague = true });
+
+            _existingTournaments = new BindableCollection<TournamentModel>(tournaments);
+
+            // TODO - Delete this dummy data when the real data from the database is ready
+        }
+    }
+    
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public bool IsLeague { get; set; }
+
+        // TODO - Delete this temporary class when the real TournamentModel class is ready
     }
 }
