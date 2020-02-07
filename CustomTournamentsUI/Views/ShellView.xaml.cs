6437828d56ad003425b65ee6a0f8b57c4d494737
Nemanja Dicic <nemanja.dicic@gmail.com>
2020-02-07 14:43:22 +0100

    Few cosmetical changes
    Changes the font on each ShellView menu buttons and font of every other View header. Makes ShellView draggable by clicking anywhere on the window surface

--- CustomTournamentsUI/Views/ShellView.xaml.cs
+++ CustomTournamentsUI/Views/ShellView.xaml.cs
@@ -28,5 +28,13 @@ namespace CustomTournamentsUI.Views
         {
             Close();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
